d890bdb62cb7b26011723ff68c362a13b4da474d
Ho Hoan Sang <sanghh>
2023-05-10 11:37:41 +0700

    setup router

--- ShopeeClone/src/App.tsx
+++ ShopeeClone/src/App.tsx
@@ -1,32 +1,10 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-// eslint-disable-next-line import/no-unresolved
-import viteLogo from '/vite.svg';
-import './App.css';
+import { Fragment } from 'react';
+import useRouteElement from './hooks/useRouteElement';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const routeElement = useRouteElement();
 
-  return (
-    <div className='bg-red-500 text-white'>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </div>
-  );
+  return <Fragment>{routeElement}</Fragment>;
 }
 
 export default App;
---
+++ ShopeeClone/src/hooks/useRouteElement.tsx
@@ -0,0 +1,32 @@
+import { useRoutes } from 'react-router-dom';
+import AuthLayout from 'src/layouts/AuthLayout';
+import Login from 'src/pages/Login';
+import ProductList from 'src/pages/ProductList';
+import Register from 'src/pages/Register';
+
+export default function useRouteElement() {
+  const elementRoute = useRoutes([
+    {
+      path: '/',
+      element: <ProductList />
+    },
+    {
+      path: '/login',
+      element: (
+        <AuthLayout>
+          <Login />
+        </AuthLayout>
+      )
+    },
+    {
+      path: '/register',
+      element: (
+        <AuthLayout>
+          <Register />
+        </AuthLayout>
+      )
+    }
+  ]);
+
+  return elementRoute;
+}
