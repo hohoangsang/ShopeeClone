56721e9f13020243d85298cb622af4fe44e31cda
Ho Hoan Sang <sanghh>
2023-05-17 18:07:46 +0700

    create Protected route

--- ShopeeClone/src/hooks/useRouteElement.tsx
+++ ShopeeClone/src/hooks/useRouteElement.tsx
@@ -1,9 +1,20 @@
-import { useRoutes } from 'react-router-dom';
+import { Navigate, Outlet, useRoutes } from 'react-router-dom';
 import AuthLayout from 'src/layouts/AuthLayout';
 import MainLayout from 'src/layouts/MainLayout';
 import Login from 'src/pages/Login';
 import ProductList from 'src/pages/ProductList';
+import Profile from 'src/pages/Profile';
 import Register from 'src/pages/Register';
+
+const isAuthentication = false;
+
+function ProtectedRoute() {
+  return isAuthentication ? <Outlet /> : <Navigate to={'/login'} />; //chưa dăng nhập thì đá ra màn hình login, đã đăng nhập thì tiếp tục render route con
+}
+
+function RejectedRoute() {
+  return !isAuthentication ? <Outlet /> : <Navigate to={'/'} />; //đã dăng nhập thì đá ra màn hình chính, chưa đăng nhập thì tiếp tục render route con
+}
 
 export default function useRouteElement() {
   const elementRoute = useRoutes([
@@ -14,6 +25,20 @@ export default function useRouteElement() {
           <ProductList />
         </MainLayout>
       )
+    },
+    {
+      path: '',
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: '/profile',
+          element: (
+            <MainLayout>
+              <Profile />
+            </MainLayout>
+          )
+        }
+      ]
     },
     {
       path: '/login',
