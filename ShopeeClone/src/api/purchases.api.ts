7a84f005accdf3c9458a3158b14629b72b96dae0
Hồ Hoàng Sang <hoangsangho93@gmail.com>
2023-06-27 23:18:58 +0700

    set up api & type purchases

--- ShopeeClone/src/api/purchases.api.ts
+++ ShopeeClone/src/api/purchases.api.ts
@@ -1,1 +1,25 @@
+import { ProductCart, Purchases, PurchaseListStatus, PurchaseStatus } from 'src/types/purchases.type';
+import api from './api';
+import { ResponseSuccessType } from 'src/types/utils.type';
+
 const URL = '/purchases';
+
+export const purchasesApi = {
+  getPurchases: (params: { status: PurchaseListStatus }) => {
+    return api.get<ResponseSuccessType<Purchases[]>>(URL, { params });
+  },
+  addToCart: (body: ProductCart) => {
+    return api.post<ResponseSuccessType<Purchases>>(`${URL}/add-to-cart`, body);
+  },
+  updatePurchases: (body: ProductCart) => {
+    return api.put(`${URL}/update-purchase`, body);
+  },
+  deletePurchases: (data: string[]) => {
+    return api.delete(URL, {
+      data
+    });
+  },
+  buyPurchases: (body: ProductCart[]) => {
+    return api.post(`${URL}/buy-products`, body);
+  }
+};
