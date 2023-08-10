81846d25bfed98cb129a4ce1cdb61c46662ba211
Ho Hoan Sang <sanghh>
2023-08-10 22:21:30 +0700

    tab status pápurchase

--- ShopeeClone/src/pages/User/pages/ChangePassword/ChangePassword.tsx
+++ ShopeeClone/src/pages/User/pages/ChangePassword/ChangePassword.tsx
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 export default function ChangePassword() {
-  return <div>ChangePassword</div>;
+  return (
+    <div className='bg-white px-6 pb-16 pt-4 text-sm shadow-sm'>
+      <div>
+        <h1 className='text-lg uppercase'>Đổi mật khẩu</h1>
+        <h1 className='text-gray-600'>Quản lý thông tin hồ sơ để bảo mật tài khoản</h1>
+      </div>
+
+      <div className='my-4 h-[1px] w-full bg-gray-200' />
+
+    </div>
+  )
 }
--- ShopeeClone/src/pages/User/pages/PurchaseHistory/PurchaseHistory.tsx
+++ ShopeeClone/src/pages/User/pages/PurchaseHistory/PurchaseHistory.tsx
@@ -1,5 +1,63 @@
-import React from 'react';
+import classNames from 'classnames';
+import { Link, createSearchParams } from 'react-router-dom';
+import { path } from 'src/constants/path';
+import { purchasesStatus } from 'src/constants/purchases';
+import { useQueryParams } from 'src/hooks/useQueryParams';
 
 export default function PurchaseHistory() {
-  return <div>PurchaseHistory</div>;
+  const params = useQueryParams();
+
+  const { status = purchasesStatus.all } = params;
+
+  return (
+    <div>
+      <div className='flex items-center justify-center'>
+        <Link
+          to={{
+            pathname: path.purchaseHistory,
+            search: createSearchParams({
+              ...params,
+              status: String(purchasesStatus.all)
+            }).toString()
+          }}
+          className={classNames('flex-1 bg-white py-4 text-center border-b-[2px]', {
+            'border-b-orange': Number(status) === purchasesStatus.all,
+            'border-b-gray-300': Number(status) !== purchasesStatus.all
+          })}
+        >
+          Tất cả
+        </Link>
+        <Link
+          to={{
+            pathname: path.purchaseHistory,
+            search: createSearchParams({
+              ...params,
+              status: String(purchasesStatus.waitForConfirmation)
+            }).toString()
+          }}
+          className={classNames('flex-1 bg-white py-4 text-center border-b-[2px]', {
+            'border-b-orange': Number(status) === purchasesStatus.waitForConfirmation,
+            'border-b-gray-300': Number(status) !== purchasesStatus.waitForConfirmation
+          })}
+        >
+          Chờ xác nhận
+        </Link>
+        <Link
+          to={{
+            pathname: path.purchaseHistory,
+            search: createSearchParams({
+              ...params,
+              status: String(purchasesStatus.waitForPicking)
+            }).toString()
+          }}
+          className={classNames('flex-1 bg-white py-4 text-center border-b-[2px]', {
+            'border-b-orange': Number(status) === purchasesStatus.waitForPicking,
+            'border-b-gray-300': Number(status) !== purchasesStatus.waitForPicking
+          })}
+        >
+          Chờ lấy hàng
+        </Link>
+      </div>
+    </div>
+  );
 }
