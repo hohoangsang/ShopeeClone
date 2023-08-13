4c5d41b38a5bd7d22cbfa80c9d46b20b02536879
Hồ Hoàng Sang <hoangsangho93@gmail.com>
2023-08-13 15:40:04 +0700

    logic doi mat khau

--- ShopeeClone/src/utils/rules.ts
+++ ShopeeClone/src/utils/rules.ts
@@ -57,25 +57,29 @@ export const getRules = (getValues?: UseFormGetValues<any>): RulesType => ({
   }
 });
 
+const handleConfirmPasswordYup = (refString: string) => {
+  return yup
+    .string()
+    .required('Vui lòng nhập password!')
+    .min(5, 'Độ dài từ 5-160 ký tự')
+    .max(160, 'Đồ dài từ 5-160 ký tự')
+    .oneOf([yup.ref(refString)], 'Nhập lại mật khẩu không khớp!');
+};
+
 export const schema = yup.object({
   email: yup
     .string()
-    .required('Please enter email!')
+    .required('Vui lòng nhập email!')
     // .email('Invalid email!')
     .matches(/^[\w-]+@([\w-]+\.)+[\w-]{2,4}$/, 'Invalid email!')
-    .min(5, 'Length is 5-160 characters')
-    .max(160, 'Length is 5-160 characters'),
+    .min(5, 'Độ dài từ 5-160 ký tự')
+    .max(160, 'Độ dài từ 5-160 ký tự'),
   password: yup
     .string()
-    .required('Please enter password!')
-    .min(6, 'Length is 6-160 characters')
-    .max(160, 'Length is 6-160 characters'),
-  confirm_password: yup
-    .string()
-    .required('Please enter password!')
-    .min(5, 'Length is 5-160 characters')
-    .max(160, 'Length is 5-160 characters')
-    .oneOf([yup.ref('password')], 'Confirm password not match with password!'),
+    .required('Vui lòng nhập password!')
+    .min(6, 'Độ dài từ 6-160 ký tự')
+    .max(160, 'Độ dài từ 6-160 ký tự'),
+  confirm_password: handleConfirmPasswordYup('password'),
   price_min: yup.string().test({
     name: 'price-not-allow',
     message: 'Giá không hợp lệ',
@@ -106,15 +110,20 @@ export const schema = yup.object({
 });
 
 export const userSchema = yup.object({
-  name: yup.string().max(160, "Đồ dài tối đa là 160 ký tự"),
-  phone: yup.string().max(20, "Đồ dài tối đa là 20 ký tự"),
-  address: yup.string().max(20, "Đồ dài tối đa là 160 ký tự"),
-  date_of_birth: yup.date().max(new Date(), "Vui lòng chọn ngày trong quá khứ"),
-  avatar: yup.string().max(1000, "Đồ dài tối đa là 1000 ký tự"),
-  password: schema.fields['password'],
-  new_password: schema.fields['password'],
-  confirm_password: schema.fields['confirm_password'],
-})
+  name: yup.string().max(160, 'Đồ dài tối đa là 160 ký tự'),
+  phone: yup.string().max(20, 'Đồ dài tối đa là 20 ký tự'),
+  address: yup.string().max(20, 'Đồ dài tối đa là 160 ký tự'),
+  date_of_birth: yup.date().max(new Date(), 'Vui lòng chọn ngày trong quá khứ'),
+  avatar: yup.string().max(1000, 'Đồ dài tối đa là 1000 ký tự'),
+  password: schema.fields['password'] as yup.StringSchema<string | undefined, yup.AnyObject, undefined, ''>,
+  new_password: schema.fields['password'] as yup.StringSchema<string | undefined, yup.AnyObject, undefined, ''>,
+  confirm_password: handleConfirmPasswordYup('new_password') as yup.StringSchema<
+    string | undefined,
+    yup.AnyObject,
+    undefined,
+    ''
+  >
+});
 
 export type UserSchema = yup.InferType<typeof userSchema>;
 
