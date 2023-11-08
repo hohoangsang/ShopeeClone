09c33a6141ee124c8043f971019ff8bff6636eb6
Hồ Hoàng Sang <hoangsangho93@gmail.com>
2023-11-08 22:44:59 +0700

    Fix warning no-unused-var eslint & tsconfig

--- ShopeeClone/.eslintrc.cjs
+++ ShopeeClone/.eslintrc.cjs
@@ -38,9 +38,12 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     // Cảnh báo khi thẻ <a target='_blank'> mà không có rel="noreferrer"
     'react/jsx-no-target-blank': 'warn',
-    // Tăng cường một số rule prettier (copy từ file .prettierrc qua)
+
+    // Bỏ qua việc check những biến và params đc định nghĩ nhưng ko được sử dụng
+    // Mục đích để khi build production sẽ bỏ qua lỗi này và tiếp tục build
     'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    // Tăng cường một số rule prettier (copy từ file .prettierrc qua)
     'prettier/prettier': [
       'warn',
       {
