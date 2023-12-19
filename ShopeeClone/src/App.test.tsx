5283edd5acd6b4b796df7a1032c4d5abb975ff57
hohoangsang <hoangsangho93@gmail.com>
2023-12-19 23:15:16 +0700

    add custom test

--- ShopeeClone/src/App.test.tsx
+++ ShopeeClone/src/App.test.tsx
@@ -5,7 +5,7 @@ import '@testing-library/jest-dom/vitest';
 import { BrowserRouter, MemoryRouter } from 'react-router-dom';
 import App from './App';
 import { afterEach } from 'node:test';
-import { logScreen } from './utils/test';
+import { customTest, logScreen } from './utils/test';
 
 describe('Test App', () => {
   afterEach(() => {
@@ -19,77 +19,97 @@ describe('Test App', () => {
    * callback sẽ dừng khi hết timeout hoặc callback thực thi xong
    */
 
-  describe('Test verify homepage & header & fotter', () => {
-    render(<App />, {
+  test('Test verify homepage & header & footer', async () => {
+    const { unmount } = render(<App />, {
       wrapper: BrowserRouter
     }); //Render App trong môi trường nodejs;
-    test('Verify Render Homepage', async () => {
-      //Test title of the page
-      await waitFor(
-        () => {
-          expect(document.querySelector('title')?.textContent).toBe('Shopee Clone | Ho Hoang Sang');
-        },
-        { timeout: 5000 }
-      );
-    });
-
-    test('Test render Header component', async () => {
-      await waitFor(
-        () => {
-          expect(document.getElementsByTagName('header')[0]).toBeInTheDocument();
-        },
-        { timeout: 2000 }
-      );
-    });
-    test('Test render Footer component', async () => {
-      await waitFor(
-        () => {
-          expect(document.querySelector('footer')).toHaveTextContent(/© 2023 Shopee. Tất cả các quyền được bảo lưu/i);
-          expect(document.getElementsByTagName('footer')[0]).toBeInTheDocument();
-        },
-        { timeout: 2000 }
-      );
-    });
-  });
-
-  // test('Test render render app and trigger action navigate to Login/Register page', async () => {
-  //   const loginBtn = screen.getByText(/Đăng nhập/i);
-
-  //   if (loginBtn) {
-  //     await userEvent.click(loginBtn);
-  //     await waitFor(
-  //       () => {
-  //         expect(document.querySelector('title')?.textContent).toBe('Đăng nhập | Shopee Clone');
-  //         expect(document.querySelector('header')?.textContent).toMatch(/Đăng nhập/i);
-  //         expect(screen.queryByText(/Bạn chưa có tài khoản?/i)).toBeInTheDocument();
-  //       },
-  //       { timeout: 2000 }
-  //     );
-  //   }
-  //   // const registerBtn = screen.getByText(/Đăng ký/i);
-
-  //   // if (registerBtn) {
-  //   //   await userEvent.click(registerBtn);
-  //   //   await waitFor(
-  //   //     () => {
-  //   //       expect(document.querySelector('title')?.textContent).toBe('Đăng ký | Shopee Clone');
-  //   //       expect(document.querySelector('header')?.textContent).toMatch(/Đăng ký/i);
-  //   //       expect(screen.queryByText(/Bạn đã có tài khoản?/i)).toBeInTheDocument();
-  //   //     },
-  //   //     { timeout: 2000 }
-  //   //   );
-  //   // }
-  // });
-
-  test('Test page 404', async () => {
-    const badRoute = '/123/badroute';
-
-    render(
-      <MemoryRouter initialEntries={[badRoute]}>
-        <App />
-      </MemoryRouter>
+    //Test title of the page
+    await waitFor(
+      () => {
+        expect(document.querySelector('title')?.textContent).toBe('Shopee Clone | Ho Hoang Sang');
+      },
+      { timeout: 5000 }
     );
 
-    await logScreen();
+    //Test header
+    await waitFor(
+      () => {
+        expect(document.getElementsByTagName('header')[0]).toBeInTheDocument();
+      },
+      { timeout: 2000 }
+    );
+
+    //Test footer
+    await waitFor(
+      () => {
+        expect(document.querySelector('footer')).toHaveTextContent(/© 2023 Shopee. Tất cả các quyền được bảo lưu/i);
+        expect(document.getElementsByTagName('footer')[0]).toBeInTheDocument();
+      },
+      { timeout: 2000 }
+    );
+    unmount();
+  });
+
+  test('Test render render app and trigger action navigate to Login/Register page', async () => {
+    const { unmount } = render(<App />, {
+      wrapper: BrowserRouter
+    });
+
+    const loginBtn = screen.getByText(/Đăng nhập/i);
+    if (loginBtn) {
+      await userEvent.click(loginBtn);
+      await waitFor(
+        () => {
+          expect(document.querySelector('title')?.textContent).toBe('Đăng nhập | Shopee Clone');
+          expect(document.querySelector('header')?.textContent).toMatch(/Đăng nhập/i);
+          expect(screen.queryByText(/Bạn chưa có tài khoản?/i)).toBeInTheDocument();
+        },
+        { timeout: 2000 }
+      );
+
+      // await logScreen();
+    }
+    // const registerBtn = screen.getByText(/Đăng ký/i);
+    // if (registerBtn) {
+    //   await userEvent.click(registerBtn);
+    //   await waitFor(
+    //     () => {
+    //       expect(document.querySelector('title')?.textContent).toBe('Đăng ký | Shopee Clone');
+    //       expect(document.querySelector('header')?.textContent).toMatch(/Đăng ký/i);
+    //       expect(screen.queryByText(/Bạn đã có tài khoản?/i)).toBeInTheDocument();
+    //     },
+    //     { timeout: 2000 }
+    //   );
+    // }
+
+    unmount();
+  });
+
+  // test('Test page 404', async () => {
+  //   const badRoute = '/123/badroute';
+
+  //   const { unmount } = render(
+  //     <MemoryRouter initialEntries={[badRoute]}>
+  //       <App />
+  //     </MemoryRouter>
+  //   );
+
+  //   await logScreen();
+
+  //   unmount();
+  // });
+
+  customTest({
+    title: 'Test page 404',
+    document: {
+      ui: (
+        <MemoryRouter initialEntries={['/123/badroute']}>
+          <App />
+        </MemoryRouter>
+      )
+    },
+    callBack: async () => {
+      await logScreen();
+    }
   });
 });
--- ShopeeClone/src/utils/test.ts
+++ ShopeeClone/src/utils/test.ts
@@ -1,5 +1,6 @@
-import { screen, waitFor, type waitForOptions } from '@testing-library/react';
-import { expect } from 'vitest';
+import { render, screen, waitFor, type waitForOptions } from '@testing-library/react';
+import { Fragment, ReactElement } from 'react';
+import { expect, test } from 'vitest';
 
 const delay = (time: number) =>
   new Promise((resolve) => {
@@ -24,4 +25,25 @@ export const logScreen = async (
   );
 
   screen.debug(node, 9999999);
+};
+
+type CustomTest = {
+  title: string;
+  document: { wrapper?: React.JSXElementConstructor<{ children: React.ReactElement }>; ui: ReactElement };
+  callBack: () => Promise<void>;
+};
+
+export const customTest = (object: CustomTest) => {
+  const { title, callBack, document } = object;
+  const { ui, wrapper } = document;
+
+  return test(title, async () => {
+    const { unmount } = render(ui, {
+      wrapper: wrapper || Fragment
+    });
+
+    await callBack();
+
+    unmount();
+  });
 };
